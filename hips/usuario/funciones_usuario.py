2802acd7b8047ed64cc92ffdf7913b981a508d89
bamy16 <60363312+bamy16@users.noreply.github.com>
2021-07-14 11:01:39 -0400

    Add función para encontrar ip de un usuario

--- hips/usuario/funciones_usuario.py
+++ hips/usuario/funciones_usuario.py
@@ -4,28 +4,40 @@ import subprocess
 
 
 
-
+# Funciones para el usuario
 def eliminar_usuario(nombre_usuario):
+    # Elimina un usuario del sistema
     if nombre_usuario != 'root' :
         os.system("sudo userdel -r sammy")
 
-                
-# Cambiar contrasena de usuario por una aleatoria y enviar por correo
 def cambiar_contrasena(nombre_usuario):
+    # Cambia la contrasena de  un usuario por una aleatoria 
     c = "openssl rand -base64 15"
     p1 = subprocess.Popen(c, shell= True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
     contrasena, err = p1.communicate() 
     cmd = 'sudo usermod -p `perl -e "print crypt("'+contrasena+'","Q4")"` '+nombre_usuario
     os.system(cmd)
 
-# Cierra la sesion de un usuario
 def cerrar_sesion(nombre_usuario):
+    # Cierra la sesion de un usuario
     cmd = "pkill -KILL -u " + str(nombre_usuario)
     os.system(cmd)
 
-    
-# Bloquear un usuario por usermod
 def bloquear_usuario(nombre_usuario):
+    # Bloquear un usuario por usermod
     if nombre_usuario != "root": # No se permite bloquear al usuario root              
         cmd="sudo usermod -L " + str(nombre_usuario)
         os.system(cmd)
+
+def encontrar_ip(nombre_usuario):
+    # Retorna el ip de un usuario a traves del nombre de usuario
+    cmd = "who | awk '{print($5)}' | sed 's/(//g' | sed 's/)//g'"  
+    p1 = subprocess.Popen(cmd, shell= True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    conectados, err = p1.communicate() 
+    lista = conectados.split('\n')
+    ip = ''
+    for usuarios in lista:
+        usuario = usuarios.split(' ')
+        if usuario[0] == nombre_usuario:
+            ip = usuario[1]
+    return ip
