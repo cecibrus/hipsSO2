f9c3d296f883dfc4b46e567ea27da94f2447d315
bamy16 <60363312+bamy16@users.noreply.github.com>
2021-07-18 12:14:29 -0400

    Update funciones_cron.py

--- hips/cron/funciones_cron.py
+++ hips/cron/funciones_cron.py
@@ -5,11 +5,14 @@ def eliminar_cron(usuario):
     cmd = "sudo crontab -r -u" + usuario 
     os.system(cmd)
 
-def listar_crontabs_en_ejecucion():
+def listar_arhivos_crontabs():
     cmd = "ls /var/spool/cron/*"
     p1 = subprocess.Popen(cmd, shell= True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
     crontabs, err = p1.communicate() 
     lista = crontabs.split('\n')
-    return lista
-
-
+    result = []
+    for cron in lista:
+        aux = cron.split('cron/')
+        if aux != [''] :
+            result.append(aux[1])
+    return result
