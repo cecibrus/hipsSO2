ed2edf877e4af589ab10f1fe8f3390f8cebdf73d
bamy16 <60363312+bamy16@users.noreply.github.com>
2021-07-18 12:16:36 -0400

    Update menu_principal.py

--- hips/menu_principal.py
+++ hips/menu_principal.py
@@ -1,2 +1,8 @@
+import sys
+sys.path.insert(0, '/root/hips/cron')
+from analiza_cron import analizar_crontabs
+sys.path.insert(0, '/root/hips/base_de_datos')
 from base_de_datos import creacion_bd
-creacion_bd()
+
+inicializar_bd()
+analizar_crontabs()
