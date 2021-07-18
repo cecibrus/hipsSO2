358bd4efc762712e7b88791d831fe8f67bdd61e9
bamy16 <60363312+bamy16@users.noreply.github.com>
2021-07-18 12:13:38 -0400

    Update funciones_bd.py

--- hips/base_de_datos/funciones_bd.py
+++ hips/base_de_datos/funciones_bd.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.insert(0, '/root/hips')
 from definiciones import BD_CONEXION
 import psycopg2
 from psycopg2 import extras
@@ -6,17 +8,12 @@ from psycopg2 import sql
 
 # - - - - -  usuario_registro  - - - - - -
 
-def add_usuario_registro_bd():
+def add_usuario_registro_bd(lista_usuario):
     # Agregar informacion de nuevo usuario
-    print("Agregar un nuevo usuario")
-    print("[nombre_usuario]: ")
-    nombre_usuario = input()
-    print("[ubicacion_permitida]: ")
-    ip_permitida = input()
-    print("[dias_permitidos]: (Formato: x-x-x-x-x...)")
-    dias_permitidos = input()  
-    print("[rango_horario_permitido]  (Formato: xx:xx-xx:xx): ")
-    rango_horario_permitido = input() 
+    nombre_usuario = lista_usuario[0]
+    ip_permitida = lista_usuario[1]
+    dias_permitidos = lista_usuario[2]
+    rango_horario_permitido = lista_usuario[3]
     if len(nombre_usuario)>0 and len(ip_permitida)>0 and len(dias_permitidos)>0 and len(rango_horario_permitido)>0 :
         tupla = (str(nombre_usuario),str(ip_permitida),str(dias_permitidos),str(rango_horario_permitido))
         insert_data='INSERT INTO usuario_registro(nombre_usuario, ip_permitida, dias_permitidos, rango_horario_permitido) VALUES({},{},{},{}) returning nombre_usuario;'
@@ -57,34 +54,28 @@ def obtener_lista_cron_bd():
     dbConnection.close()
     return lista_cron
 
-def add_cron_bd():
+def add_cron_bd(lista_cron):
     # Agregar cron a la lista blanca de crontabs
-    print("Crontab con restriccion: (si o no)")
-    restriccion = input()
-    print("Configuración de tiempo: (solo para comandos con restricciones)")
-    print("[m h dom mon dow]: ")
-    config_cron = input()
-    print("[user]: ")
-    usuario_cron = input()
-    print("[comand]:")
-    comando = input()  
+    restriccion = lista_cron[0]
+    config_cron = lista_cron[1]
+    comando = lista_cron[2]
     if restriccion == 'si':
-        if len(config_cron)>0 and len(usuario_cron)>0 and len(comando)>0:
+        if len(config_cron)>0 and len(comando)>0:
             ban =1
-    elif len(restriccion)>0 and len(usuario_cron)>0 and len(comando)>0 :
+    elif len(restriccion)>0 and len(comando)>0 :
         ban = 1 
     else:
         print("No agregado")
     if ban == 1:
-        tupla = (str(restriccion),str(config_cron),str(usuario_cron),str(comando))
-        insert_data='INSERT INTO cron(restriccion, config_cron, usuario_cron, comando) VALUES({},{},{},{}) returning id_cron;'
+        tupla = (str(restriccion),str(config_cron), str(comando))
+        insert_data='INSERT INTO cron(restriccion, config_cron, comando) VALUES({},{},{}) returning id_cron;'
         dbConnection = psycopg2.connect(BD_CONEXION)
         cursor = dbConnection.cursor()
-        cursor.execute(sql.SQL(insert_data.format(tupla[0], tupla[1], tupla[2], tupla[3])))
+        cursor.execute(sql.SQL(insert_data.format(tupla[0], tupla[1], tupla[2])))
         cursor.commit()
         cursor.close()
 
-def delete_crontab_bd(usuario):
+def delete_crontab_bd():
     # elimina crontab de la tabla cron
     dbConnection = psycopg2.connect(BD_CONEXION)
     dbConnection.execute("DELETE FROM cron")
@@ -95,13 +86,15 @@ def delete_crontab_bd(usuario):
 
 # - - - - -  alarma  - - - - - -
 
-def add_alarma_bd(fecha,alarma):
+def add_alarma_bd(fecha,mensaje):
     # agrega un alarma a la tabla alarma
-    if len(alarma)>0 and len(fecha)>0:
+    if len(mensaje)>0 and len(fecha)>0:
+        print("Add alarma bd entro")
         dbConnection = psycopg2.connect(BD_CONEXION)
         cursor = dbConnection.cursor()
-        insert_data='INSERT INTO alarma(fecha, mensaje) VALUES({},{}) returning id_alarma;'
-        cursor.execute(sql.SQL(insert_data.format(fecha,alarma)))
+        insert_data='INSERT INTO alarma(fecha, mensaje) VALUES({},{});'
+        cursor.execute(sql.SQL(insert_data.format("'" + fecha + "'","'" + mensaje + "'")))
+        print(cursor.fetchall())
         dbConnection.commit()
         dbConnection.close()
 
@@ -109,7 +102,7 @@ def add_prevencion_bd(fecha, mensaje):
     if len(fecha)>0 and len(mensaje)>0:
         dbConnection = psycopg2.connect(BD_CONEXION)
         cursor = dbConnection.cursor()
-        insert_data='INSERT INTO prevencion(fecha, mensaje) VALUES({},{}) returning id_prevencion;'
-        cursor.execute(sql.SQL(insert_data.format(fecha,mensaje)))
+        insert_data='INSERT INTO prevencion(fecha, mensaje) VALUES({},{});'
+        cursor.execute(sql.SQL(insert_data.format("'" + fecha + "'" + mensaje + "'")))
         dbConnection.commit()
         dbConnection.close()
