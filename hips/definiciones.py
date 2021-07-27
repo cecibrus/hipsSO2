f5eea5b7d3ba72de28f6e23c3e0bdd81f7896960
bamy16 <60363312+bamy16@users.noreply.github.com>
2021-07-27 17:18:38 -0400

    Update definiciones.py

--- hips/definiciones.py
+++ hips/definiciones.py
@@ -1,5 +1,12 @@
 import os
 
 BD_CONEXION =  "dbname=hips user=postgres password=Hips2021 host=localhost"
-CORREO = "so2.hips2021@gmail.com"
-
+MAX_INTENTOS_LOGIN_SU = 20
+MAX_INTENTOS_LOGIN_SSHD = 20
+MAX_INTENTOS_LOGIN_SMTP = 20
+MAX_ACCES_LOG = 20
+MAX_CORREOS_POR_DIA = 100
+MAX_DNS_COUNT = 20
+MAX_CPU = 10
+MAX_MEM = 10
+IP_LOCAL = "192.168.0.7"
