c856313fdb2c5410d959e2b0c6fd552a4a974c2e
bamy16 <60363312+bamy16@users.noreply.github.com>
2021-07-28 10:38:54 -0400

    Add files via upload

--- hips/interfaz_web/actualizar.php
+++ hips/interfaz_web/actualizar.php
@@ -1,65 +1,110 @@
 <?php
-echo "we in\n";
-session_start();
-$dbconn = pg_connect($_SESSION['CONN']);
-$updateQuery = '';
-if(isset($_POST['addDangerapp'])){
-	$name = $_POST['appname'];
-	$updateQuery = "INSERT INTO dangerapp (name) VALUES ('$name');";
-	unset($_POST['addDangerapp']);
-	echo "danger done";
-}elseif(isset($_POST['addProcesslimits'])){
-	$processName = $_POST['processname'];
-	$maxCpu = $_POST['cpumax'];
-	$maxMem = $_POST['memmax'];
-	$maxTime = $_POST['maxtime'];
-	$updateQuery = "INSERT INTO processlimits (name,cpu,mem,maxtime) VALUES ('$processName','$maxCpu','$maxMem','$maxTime');";
-	unset($_POST['addProcesslimits']);
-	echo "process done";
-}elseif(isset($_POST['addGeneral'])){
-	$ipv4 = $_POST['ipv4'];
-	$maxmailq = $_POST['maxmailq'];
-	$email = $_POST['email'];
-	$email_pass = $_POST['email_pass'];
-	$max_ssh = $_POST['max_ssh'];
-	$max_fuzz = $_POST['max_fuzz'];
-	$max_mail_pu = $_POST['max_mail_pu'];
-	$default_max_cpu = $_POST['max_cpu_def'];
-	$default_max_mem = $_POST['max_mem_def'];
-	$updateQuery = "DElETE FROM general WHERE myipv4 IS NOT NULL;INSERT INTO general (myipv4,maxmailq,email,email_pass,max_ssh,max_fuzz,max_mail_pu,default_max_cpu,default_max_mem) VALUES ('$ipv4','$maxmailq','$email','$email_pass','$max_ssh','$max_fuzz','$max_mail_pu','$default_max_cpu','$default_max_mem');";
-	unset($_POST['addGeneral']);
-	echo "general done";
-}elseif(isset($_POST['addMd5sum'])){
-	$dir = $_POST['filedir'];
-	$hash = $_POST['hash'];
-	$updateQuery = "INSERT INTO md5sum (dir,hash) VALUES ('$dir','$hash');";
-	unset($_POST['addMd5sum']);
-	echo "md5sum done";
-}elseif(isset($_POST['delDangerapp'])){
-	$nameD = $_POST['delnameD'];
-	$updateQuery = "DELETE FROM dangerapp WHERE name = '$nameD';";
-	unset($_POST['delDangerapp']);
-	echo "del danger done";
-}elseif(isset($_POST['delProcesslimits'])){
-	$nameP = $_POST['delnameP'];
-	$updateQuery = "DELETE FROM processlimits WHERE name = '$nameP';";
-	unset($_POST['delProcesslimits']);
-	echo "del process done";
-}elseif(isset($_POST['delMd5sum'])){
-	$dirM = $_POST['delnameM'];
-	$updateQuery = "DELETE FROM md5sum WHERE dir = '$dirM';";
-	unset($_POST['addMd5sum']);
-	echo "del md5sum done";
-}else{
-	echo "Oops...Algo salio mal.";
-	exit;
-}
+	session_start();
+	if(isset($_SESSION['usuario']) && !empty($_SESSION['usuario'])){
+		$dbconn = pg_connect($_SESSION['CONN']);
+		if (!$dbconn){
+			echo "no se conecta";
+			exit;
+		}
+	}
+	$query = '';
+	$result = '';
+	if(isset($_POST['addUsuarioRegistro'])){
+		$nombre_usuario = $_POST['nombre_usuario'];
+		$ip_permitida = $_POST['ip_permitida'];
+		$dias_permitidos = $_POST['dias_permitidos'];
+		$rango_horario_permitido = $_POST['rango_horario_permitido'];
 
-$result = pg_query($dbconn, $updateQuery);
-if (!$result){
-	echo "Ocurrio un error. Verifique la conexion a la BD y vuelva a intentarlo.\n";
-	exit;
-}
-pg_close($dbconn);
-header("Location: welcome.php");
+		$query = "INSERT INTO usuario_registro(nombre_usuario, ip_permitida, dias_permitidos, rango_horario_permitido) VALUES ('$nombre_usuario','$ip_permitida','$dias_permitidos','$rango_horario_permitido');";
+		unset($_POST['addUsuarioRegistro']);
+		echo "Nuevo usuario registrado";
+
+	}elseif(isset($_POST['addCron'])){
+		$restriccion = $_POST['restriccion'];
+		$config_cron = $_POST['config_cron'];
+		$comando = $_POST['comando'];
+		$query = "INSERT INTO cron(restriccion,config_cron,comando) VALUES ('$restriccion','$config_cron','$comando');";
+		unset($_POST['addCron']);
+		echo "Nuevo Crontab registrado";
+
+	}elseif(isset($_POST['addAlarma'])){
+		$fecha = $_POST['fecha'];
+		$mensaje = $_POST['mensaje'];
+		
+		$query = "INSERT INTO alarma(fecha,mensaje) VALUES ('$fecha','$mensaje');";
+		unset($_POST['addAlarma']);
+		echo "Nuevo reporte de alarma registrado";
+
+	}elseif(isset($_POST['addPrevencion'])){
+		$fecha = $_POST['fecha'];
+		$mensaje = $_POST['mensaje'];
+		
+		$query = "INSERT INTO prevencion(fecha,mensaje) VALUES ('$fecha','$mensaje');";
+		unset($_POST['addPrevencion']);
+		echo "Nuevo reporte de prevencion registrado";
+
+	}elseif(isset($_POST['addCorreoConfig'])){
+		$fecha = $_POST['usuario_nombre'];
+		$mensaje = $_POST['mail'];
+		$mensaje = $_POST['passwd'];
+
+		$query = "INSERT INTO correo_config(usuario_nombre,mail,passwd) VALUES ('$usuario_nombre','$mail','$passwd');";
+		unset($_POST['addCorreoConfig']);
+		echo "Nuevo registro de configuracion de correo registrado";
+
+	}elseif(isset($_POST['lista_negra_correo'])){
+		$fecha = $_POST['correo'];
+
+		$query = "INSERT INTO lista_negra_correo(correo) VALUES ('$correo');";
+		unset($_POST['addCorreoListaNegra']);
+		echo "Nuevo registro de correo en lista negra registrado";
+
+	}elseif(isset($_POST['delUsuarioRegistro'])){
+		$nombre_usuario_d = $_POST['nombre_usuario_d'];
+		$query = "DELETE FROM usuario_registro WHERE nombre_usuario = '$nombre_usuario_d';";
+		unset($_POST['delUsuarioRegistro']);
+		echo "Registro de usuario_registro eliminado correctamente.";
+
+	}elseif(isset($_POST['delCron'])){
+		$id_cron_d = $_POST['id_cron_d'];
+		$query = "DELETE FROM cron WHERE id_cron = '$id_cron_d';";
+		unset($_POST['delCron']);
+		echo "Registro de cron eliminado correctamente.";
+
+	}elseif(isset($_POST['delAlarma'])){
+		$id_alarma_d = $_POST['id_alarma_d'];
+		$query = "DELETE FROM alarma WHERE id_alarma = '$id_alarma_d';";
+		unset($_POST['delAlarma']);
+		echo "Registro de alarma eliminado correctamente.";
+
+	}elseif(isset($_POST['delPrevencion'])){
+		$id_prevencion_d = $_POST['id_prevencion_d'];
+		$query = "DELETE FROM prevencion WHERE id_prevencion = '$id_prevencion_d';";
+		unset($_POST['delPrevencion']);
+		echo "Registro de prevencion eliminado correctamente.";
+
+	}elseif(isset($_POST['delConfigCorreo'])){
+		$id_correo_d = $_POST['id_correo_d'];
+		$query = "DELETE FROM correo_config WHERE id_correo = '$id_correo_d';";
+		unset($_POST['delConfigCorreo']);
+		echo "Registro de correo_config eliminado correctamente.";
+
+		
+	}elseif(isset($_POST['delCorreoListaNegra'])){
+		$correo_d = $_POST['correo_d'];
+		$query = "DELETE FROM lista_negra_correo WHERE correo = '$correo_d';";
+		unset($_POST['delCorreoListaNegra']);
+		echo "Registro de lista_negra_correo eliminado correctamente.";
+	}else{
+		echo "Error inesperado. Intente de nuevo ";
+		exit;
+	}
+
+	$result = pg_query($dbconn, $query);
+	if (!$result){
+		echo "Error. No se pudo realizar la consulta.\n";
+		exit;
+	}
+	pg_close($dbconn);
+	header("Location: ingreso.php");
 ?>
--- hips/interfaz_web/ingreso.php
+++ hips/interfaz_web/ingreso.php
@@ -1,315 +1,338 @@
 <?php
 	session_start();
-	if(isset($_SESSION['usuario']) && !empty($_SESSION['usuario'])):
+	if(isset($_SESSION['usuario']) && !empty($_SESSION['usuario'])){
 		$dbconn = pg_connect($_SESSION['CONN']);
+		if (!$dbconn){
+			echo "no se conecta";
+			exit;
+		}
+	}
 ?>	
-
 <html>
-	<div class="box">
-		<form action="cerrar_sesion.php" method="post">
-			<button type="submit" name="cerrar_sesion" class="warnbtn">Cerrar sesión</button>
-			<button type="submit" name="actualizar" onclick="window.location.reload();">Actualizar datos</button>
-		</form>
-	</div>
+	<head>
+    	<title>Sistema HIPS</title>
+    	<link rel="stylesheet" href="style_ingreso.css">
+		<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
 
-
-	<select id="tablabd" name="tabla" onchange="changeView()">
-		<option value="usuario_registro">Usuario Registro</option>
-		<option value="cron">Cron</option>
-		<option value="alarma"> Reporte de Alarma</option>
-		<option value="prevencion">Reporte de Prevencion</option>
-		<option value="correo">Correos Registrados</option>
-		<option value="correo_lista_negra">Correo no Deseado</option>
-	</select>
-	<div id="usuarioRegistro">
-		<br/>
-		<br/>
-		<b>Agregar fila:</b>
-		<br/>
-		<br/>
-		<form action="update.php" method="post">
-			<input type="text" placeholder="'nombre_usuario'" name="nombre_usuario" required />
-			<input type="text" placeholder="'ip_permitida'" name="ip_permitida" required />
-			<input type="text" placeholder="'dias_permitidos'" name="dias_permitidos" required />
-			<input type="text" placeholder="'rango_horario_permitido'" name="rango_horario_permitido" required />
-			<button type="submit" name="addUsuarioRegistro" class="addButton">Agregar</button>
-        </form>
-		<br/>
-		<b>Eliminar fila:</b>
-		<br/>
-		<br/>
-		<form action="update.php" method="post">
-			<input type="text" placeholder="'nombre_usuario'" name="nombre_usuario" required />
-			<input type="text" placeholder="'ip_permitida'" name="ip_permitida" required />
-			<input type="text" placeholder="'dias_permitidos'" name="dias_permitidos" required />
-			<input type="text" placeholder="'rango_horario_permitido'" name="rango_horario_permitido" required />
-
-			<button type="submit" name="delUsuarioRegistro" class="deleteButton">Eliminar</button>
-		</form>
-		<br/>
-		<br/>
-		<br/>
-		<tr>
-			<th>Tabla Usuario Registro</th>
-		</tr>
-		<?php
-			$result = pg_query($dbconn, "SELECT * FROM usuario_registro ORDER BY usuario_registro;");
-			if (!$result){echo "1";}
-			while ($row = pg_fetch_row($result)){
-				echo "<tr><td>".$row[0]."</td></tr>";
-			}
-		?>
-    </div>
-
-	<div id="cron" style="display:none">
-		<br/>
-		<br/>
-		<b>Agregar fila:</b>
-		<br/>
-		<br/>
-		<form action="update.php" method="post">
-			<input type="text" placeholder="'restriccion'" name="restriccion" required />
-			<input type="text" placeholder="'config_cron'" name="config_cron" required />
-			<input type="text" placeholder="'comando'" name="comando" required />
-			<button type="submit" name="addCron" class="addButton">Agregar</button>
-		</form>
-		<br/>
-		<b>Eliminar fila:</b>
-		<br/>
-		<br/>
-		<form action="update.php" method="post">
-			<input type="text" placeholder="'restriccion'" name="restriccion" required />
-			<input type="text" placeholder="'config_cron'" name="config_cron" required />
-			<input type="text" placeholder="'comando'" name="comando" required />
-			<button type="submit" name="delCron" class="deleteButton">Eliminar</button>
-		</form>
-		<br/>
-		<br/>
-		<br/>
-		<table style="width:100%">
-			<tr>
-				<th>Process Name</th>
-				<th>Max CPU %</th>
-				<th>Max RAM %</th>
-				<th>Max Runtime(ms)</th>
-			</tr>
-		<?php
-			$result = pg_query($dbconn, "SELECT * FROM processlimits ORDER BY name ASC;");
-			if (!$result){echo "2";}
-			while ($row = pg_fetch_row($result)){
-				echo "<tr><td>".$row[0]."</td><td>".$row[1]."</td><td>".$row[2]."</td><td>".$row[3]."</td></tr>";
-			}
-		?>
-		</table>
-
-	</div>
-
-	<div id="alarma" style="display:none">
-		<br/>
-		<br/>
-		<b>Update Info:</b>
-		<br/>
-		<br/>
-		<form action="update.php" method="post">
-			<input type="text" placeholder="'fecha'" name="fecha" required/>
-			<input type="text" placeholder="'mensaje'" name="mensaje" required/>
-			<button type="submit" name="addAlarma">Agregar</button>
-		</form>
-		</form>
-		<br/>
-		<b>Eliminar fila:</b>
-		<br/>
-		<br/>
-		<form action="update.php" method="post">
-			<input type="text" placeholder="'id'" name="delId" required />
-			<button type="submit" name="delAlarma" class="deleteButton">Eliminar</button>
-		</form>
-		<br/>
-		<br/>
-		<br/>
-		<tr>
-			<th>My IPv4</th>
-			<th>Admin Email</th>
-			<th>Admin Email Password</th>
-		</tr>
-		<?php
-			$result = pg_query($dbconn, "SELECT * FROM alarma;");
-			if (!$result){echo "3";}
-			while ($row = pg_fetch_row($result)){
-				echo "<tr><td>".$row[0]."</td><td>".$row[2]."</td><td>".$row[3]."</td></tr>";
-			}
-		?>
+  	</head>
+	<header>
+		<h1>SISTEMA HIPS</h1>
+	</header>
+	<body>
+		<div class="box">
+			<form action="cerrar_sesion.php" method="post">
+				<button type="submit" name="cerrar_sesion" class="logout">Cerrar sesión</button>
+				<button type="submit" name="actualizar" onclick="window.location.reload();">Actualizar datos</button>
+			</form>
 		</div>
-
-		<div id="prevencion" style="display:none">
-			<br/>
-			<br/>
-			<b>Agregar fila:</b>
-			<br/>
-			<br/>
-			<form action="update.php" method="post">
-				<input type="text" placeholder="'fecha'" name="fecha" required />
-				<input type="text" placeholder="'mensaje'" name="mensaje" />
-				<button type="submit" name="addPrevencion" class="addButton">Agregar</button>
-				<br/>
-			</form>
-			<br/>
-			<b>Eliminar fila:</b>
-			<br/>
-			<br/>
-			<form action="update.php" method="post">
-				<input type="text" placeholder="'fecha'" name="delFecha" required />
-				<button type="submit" name="delPrevencion" class="deleteButton">Eliminar</button>
-			</form>
-			<br/>
-			<br/>
-			<br/>
-			<table style="width:100%">
-				<tr>
-					<th>File Absolute Path</th>
-					<th>MD5SUM Generated Hash</th>
-				</tr>
-			<?php
-				$result = pg_query($dbconn, "SELECT * FROM md5sum ORDER BY dir ASC;");
-				if (!$result){echo "4";}
-				while ($row = pg_fetch_row($result)){
-					//echo "<br> <b>File Absolute Path:</b> ".$row[0]." <b>Hash:</b> ".$row[1]."<br>";
-					echo "<tr><td>".$row[0]."</td><td>".$row[1]."</td></tr>";
-				}
-			?>
-			</table>
-
-		</div>
-
-		<div id="correo_config" style="display:none">
-			<br/>
-			<br/>
-			<b>Agregar fila:</b>
-			<br/>
-			<br/>
-			<form action="update.php" method="post">_
-				<input type="text" placeholder="'usuario_nombre'" name="usuario_nombre" required />
-				<input type="text" placeholder="'mail'" name="mail" />
-				<input type="text" placeholder="'passwd'" name="passwd" />
-				<button type="submit" name="addCorreoConfig" class="addButton">Agregar</button>
-				<br/>
-			</form>
-			<br/>
-			<b>Eliminar fila:</b>
-			<br/>
-			<br/>
-			<form action="update.php" method="post">
-				<input type="text" placeholder="'usuario_nombre'" name="usuario_nombre" required />
-				<input type="text" placeholder="'mail'" name="mail" required />
-				<input type="text" placeholder="'passwd'" name="passwd" required />
-
-				<button type="submit" name="delConfigCorreo" class="deleteButton">Eliminar</button>
-			</form>
-			<br/>
-			<br/>
-			<br/>
-			<table style="width:100%">
-				<tr>
-					<th>File Absolute Path</th>
-					<th>MD5SUM Generated Hash</th>
-				</tr>
-			<?php
-				$result = pg_query($dbconn, "SELECT * FROM md5sum ORDER BY dir ASC;");
-				if (!$result){echo "4";}
-				while ($row = pg_fetch_row($result)){
-					echo "<tr><td>".$row[0]."</td><td>".$row[1]."</td></tr>";
-				}
-			?>
-			</table>
-
-		</div>
-
-
-		<div id="lista_negra_correo" style="display:none">
-			<br/>
-			<br/>
-			<b>Agregar fila:</b>
-			<br/>
-			<br/>
-			<form action="update.php" method="post">_
-				<input type="text" placeholder="'correo'" name="usuario_nombre" required />
-				<button type="submit" name="addCorreListaNegra" class="addButton">Agregar</button>
-				<br/>
-			</form>
-			<br/>
-			<b>Eliminar fila:</b>
-			<br/>
-			<br/>
-			<form action="update.php" method="post">
-				<input type="text" placeholder="'correo'" name="usuario_nombre" required />
-				<button type="submit" name="delCorreoListaNegra" class="deleteButton">Eliminar</button>
-			</form>
-			<br/>
-			<br/>
-			<br/>
-			<table style="width:100%">
-				<tr>
-					<th>File Absolute Path</th>
-					<th>MD5SUM Generated Hash</th>
-				</tr>
-			<?php
-				$result = pg_query($dbconn, "SELECT * FROM md5sum ORDER BY dir ASC;");
-				if (!$result){echo "4";}
-				while ($row = pg_fetch_row($result)){
-					echo "<tr><td>".$row[0]."</td><td>".$row[1]."</td></tr>";
-				}
-			?>
-			</table>
-
-		</div>
-
-
-	
-
 		
+		<select id="tablabd" name="tabla" class="div-toggle" data-target=".my-info-1">
+			<option value="ninguno" data-show=".ninguno">Ninguno</option>
+			<option value="usuario_registro" data-show=".usuario_registro">Usuario Registro</option>
+			<option value="cron" data-show=".cron">Cron</option>
+			<option value="alarma" data-show=".alarma"> Reporte de Alarma</option>
+			<option value="prevencion" data-show=".prevencion">Reporte de Prevencion</option>
+			<option value="correo_config" data-show=".correo_config">Correos Registrados</option>
+			<option value="correo_lista_negra" data-show=".correo_lista_negra">Correo no Deseado</option>
+		</select>
+		
+		<div class="my-info-1">
+			<div id="ninguno" class="ninguno hide"> </div>
+			<div id="usuario_registro" class="usuario_registro hide">
+				<br/>
+				<br/>
+				<b>Agregar fila:</b>
+				<br/>
+				<br/>
+				<form action="actualizar.php" method="post">
+					<input type="text" placeholder="'nombre_usuario'" name="nombre_usuario" required />
+					<input type="text" placeholder="'ip_permitida'" name="ip_permitida" required />
+					<input type="text" placeholder="'dias_permitidos'" name="dias_permitidos" required />
+					<input type="text" placeholder="'rango_horario_permitido'" name="rango_horario_permitido" required />
+					<button type="submit" name="addUsuarioRegistro" class="addButton">Agregar</button>
+				</form>
+				<br/>
+				<b>Eliminar fila:</b>
+				<br/>
+				<br/>
+				<form action="actualizar.php" method="post">
+					<input type="text" placeholder="'nombre_usuario'" name="nombre_usuario_d" required />
+					<button type="submit" name="delUsuarioRegistro" class="deleteButton">Eliminar</button>
+				</form>
+				<br/>
+				<br/>
+				<br/>
+				<h1>Tabla usuario_registro</h1>
+				<table class="tablabd" style="width:100%">
+					<tr>
+						<th>nombre_usuario</th>
+						<th>ip_permitida</th>
+						<th>dias_permitidos</th>
+						<th>rango_horario_permitido</th>
+					</tr>
+					<?php
+						$result = pg_query($dbconn, "SELECT * FROM usuario_registro ORDER BY nombre_usuario;");
+						if ($result){
+							while($row = pg_fetch_row($result)){
+								echo "<tr>";
+								for($i=0; $i<pg_num_fields($result); $i++){
+									echo "<td>".$row[$i]."</td>";
+								}
+								echo "</tr>";
+							}
+						}else{
+							exit;
+						}
+					?>
+				</table>	
+			</div>
 
-<?php else: 
-	echo "no session";
-	header("Location: index.php");
-?>
-<?php endif;?>
+			<div id="cron" class="cron hide">
+				<br/>
+				<br/>
+				<b>Agregar fila:</b>
+				<br/>
+				<br/>
+				<form action="actualizar.php" method="post">
+					<input type="text" placeholder="'restriccion'" name="restriccion" required />
+					<input type="text" placeholder="'config_cron'" name="config_cron" required />
+					<input type="text" placeholder="'comando'" name="comando" required />
+					<button type="submit" name="addCron" class="addButton">Agregar</button>
+				</form>
+				<br/>
+				<b>Eliminar fila:</b>
+				<br/>
+				<br/>
+				<form action="actualizar.php" method="post">
+					<input type="text" placeholder="'id_cron'" name="id_cron_d" required />
+
+					<button type="submit" name="delCron" class="deleteButton">Eliminar</button>
+				</form>
+				<br/>
+				<br/>
+				<br/>
+				<h1>Tabla Cron</h1>
+				<table class="tablabd" style="width:100%">
+					<tr>
+						<th>restriccion</th>
+						<th>config_cron</th>
+						<th>comando</th>
+					</tr>
+					<?php
+						$result = pg_query($dbconn, "SELECT * FROM cron;");
+						if ($result){
+							while($row = pg_fetch_row($result)){
+								echo "<tr>";
+								for($i=0; $i<pg_num_fields($result); $i++){
+									echo "<td>".$row[$i]."</td>";
+								}
+								echo "</tr>";
+							}
+						}else{
+							exit;
+						}
+					?>
+				</table>	
+			</div>
+
+			<div id="alarma" class= "alarma hide">
+				<br/>
+				<br/>
+				<b>Agregar fila:</b>
+				<br/>
+				<br/>
+				<form action="actualizar.php" method="post">
+					<input type="text" placeholder="'fecha'" name="fecha" required/>
+					<input type="text" placeholder="'mensaje'" name="mensaje" required/>
+					<button type="submit" name="addAlarma">Agregar</button>
+				</form>
+				</form>
+				<br/>
+				<b>Eliminar fila:</b>
+				<br/>
+				<br/>
+				<form action="actualizar.php" method="post">
+					<input type="text" placeholder="'id_alarma'" name="id_alarma_d" required />
+					<button type="submit" name="delAlarma" class="deleteButton">Eliminar</button>
+				</form>
+				<br/>
+				<br/>
+				<br/>
+				<h1>Tabla Alarma</h1>
+				<table class="tablabd" style="width:100%">
+					<tr>
+						<th>id</th>
+						<th>fecha</th>
+						<th>mensaje</th>
+					</tr>
+					<?php
+						$result = pg_query($dbconn, "SELECT * FROM alarma ORDER BY id_alarma;");
+						if ($result){
+							while($row = pg_fetch_row($result)){
+								echo "<tr>";
+								for($i=0; $i<pg_num_fields($result); $i++){
+									echo "<td>".$row[$i]."</td>";
+								}
+								echo "</tr>";
+							}
+						}else{
+							exit;
+						}
+					?>
+				</table>	
+			</div>
+
+			<div id="prevencion" class="prevencion hide">
+				<br/>
+				<br/>
+				<b>Agregar fila:</b>
+				<br/>
+				<br/>
+				<form action="actualizar.php" method="post">
+					<input type="text" placeholder="'fecha'" name="fecha" required />
+					<input type="text" placeholder="'mensaje'" name="mensaje" required />
+					<button type="submit" name="addPrevencion" class="addButton">Agregar</button>
+				</form>
+				<br/>
+				<b>Eliminar fila:</b>
+				<br/>
+				<br/>
+				<form action="actualizar.php" method="post">
+					<input type="text" placeholder="'id_prevencion'" name="id_prevencion_d" required />
+					<button type="submit" name="delPrevencion" class="deleteButton">Eliminar</button>
+				</form>
+				<br/>
+				<br/>
+				<br/>
+				<h1>Tabla prevencion</h1>
+				<table class="tablabd" style="width:100%">
+					<tr>
+						<th>id</th>
+						<th>fecha</th>
+						<th>mensaje</th>
+					</tr>
+					<?php
+						$result = pg_query($dbconn, "SELECT * FROM prevencion ORDER BY id_prevencion;");
+						if ($result){
+							while($row = pg_fetch_row($result)){
+								echo "<tr>";
+								for($i=0; $i<pg_num_fields($result); $i++){
+									echo "<td>".$row[$i]."</td>";
+								}
+								echo "</tr>";
+							}
+						}else{
+							exit;
+						}
+					?>
+				</table>	
+			</div>
+
+			<div id="correo_config" class="correo_config hide">
+				<br/>
+				<br/>
+				<b>Agregar fila:</b>
+				<br/>
+				<br/>
+				<form action="actualizar.php" method="post">
+					<input type="text" placeholder="'usuario_nombre'" name="usuario_nombre" required />
+					<input type="text" placeholder="'mail'" name="mail" required />
+					<input type="text" placeholder="'passwd'" name="passwd" required />
+					<button type="submit" name="addCorreoConfig" class="addButton">Agregar</button>
+				</form>
+				<br/>
+				<b>Eliminar fila:</b>
+				<br/>
+				<br/>
+				<form action="actualizar.php" method="post">
+					<input type="text" placeholder="'id_correo'" name="id_correo_d" required />
+					<button type="submit" name="delConfigCorreo" class="deleteButton">Eliminar</button>
+				</form>
+				<br/>
+				<br/>
+				<br/>
+				<h1>Tabla correo_config</h1>
+				<table class="tablabd" style="width:100%">
+					<tr>
+						<th>id_correo</th>
+						<th>usuario_nombre</th>
+						<th>mail</th>
+						<th>passwd</th>
+					</tr>
+					<?php
+						$result = pg_query($dbconn, "SELECT * FROM correo_config ORDER BY id_correo;");
+						if ($result){
+							while($row = pg_fetch_row($result)){
+								echo "<tr>";
+								for($i=0; $i<pg_num_fields($result); $i++){
+									echo "<td>".$row[$i]."</td>";
+								}
+								echo "</tr>";
+							}
+						}else{
+							exit;
+						}
+					?>
+				</table>	
+			</div>
+
+			<div id="lista_negra_correo" class="lista_negra_correo hide">
+				<br/>
+				<br/>
+				<b>Agregar fila:</b>
+				<br/>
+				<br/>
+				<form action="actualizar.php" method="post">
+					<input type="text" placeholder="'correo'" name="correo" required />
+					<button type="submit" name="addCorreoListaNegra" class="addButton">Agregar</button>
+				</form>
+				<br/>
+				<b>Eliminar fila:</b>
+				<br/>
+				<br/>
+				<form action="actualizar.php" method="post">
+					<input type="text" placeholder="'correo'" name="correo_d" required />
+					<button type="submit" name="delCorreoListaNegra" class="deleteButton">Eliminar</button>
+				</form>
+				<br/>
+				<br/>
+				<br/>
+				<h1>Tabla lista_negra_correo</h1>
+				<table class="tablabd" style="width:100%">
+					<tr>
+						<th>correo</th>
+					</tr>
+					<?php
+						$result = pg_query($dbconn, "SELECT * FROM lista_negra_correo ;");
+						if ($result){
+							while($row = pg_fetch_row($result)){
+								echo "<tr>";
+								for($i=0; $i<pg_num_fields($result); $i++){
+									echo "<td>".$row[$i]."</td>";
+								}
+								echo "</tr>";
+							}
+						}else{
+							exit;
+						}
+					?>
+				</table>	
+			</div>	
+		</div>
+		<script>
+			$(document).on('change', '.div-toggle', function() {
+				var target = $(this).data('target');
+				var show = $("option:selected", this).data('show');
+				$(target).children().addClass('hide');
+				$(show).removeClass('hide');
+			});
+			$(document).ready(function(){
+				$('.div-toggle').trigger('change');
+			});	
+		</script>
+	</body>
 </html>
 
-<script>
-	$(document).on('change', '.tablabd', function() {
-		var target = $(this).data('target');
-		var show = $("option:selected", this).data('show');
-		$(target).children().addClass('hide');
-		$(show).removeClass('hide');
-	});
-	$(document).ready(function(){
-		$('.tablabd').trigger('change');
-	});
-</script>	
-<script>
-	function changeView(){
-		var dbtable = document.getElementById("dbtable");
-		var selectedDiv = dbtable.options[dbtable.selectedIndex].value + "Div";
-		for(var i=0; i<dbtable.length; i++){
-			var divId = dbtable.options[i].value + "Div";
-			document.getElementById(divId).style.display = "none";
-		}
-		document.getElementById(selectedDiv).style.display = "block";
-		sessionStorage.setItem("currentSelection",dbtable.options[dbtable.selectedIndex].value);
-	};
-	
-</script>
-
-<script>
-	//document.getElementById("dbtable").onchange = changeView();
-	var selStor = sessionStorage.getItem("currentSelection");
-	if (selStor != null){
-		document.getElementById("dbtable").value = selStor;
-		//window.alert(selStor);
-		changeView();
-	}
-</script>
-
 <?php
-pg_close($dbconn);
+	pg_close($dbconn);
 ?>
