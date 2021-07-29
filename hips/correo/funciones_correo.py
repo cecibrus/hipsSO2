8c3be90e987d3f591176f342246595aeb67c250f
bamy16 <60363312+bamy16@users.noreply.github.com>
2021-07-29 12:15:20 -0400

    Update funciones_correo.py

--- hips/correo/funciones_correo.py
+++ hips/correo/funciones_correo.py
@@ -1,4 +1,3 @@
-from hips.definiciones import MAX_COLA_CORREOS
 import sys
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -8,7 +7,8 @@ sys.path.insert(0, '/root/hips/base_de_datos')
 from funciones_bd import obtener_parametros_correo_bd
 sys.path.insert(0, '/root/hips/reportes')
 from reportes.reporte_alarmas_prevencion import reportar_alarma, reportar_prevencion
-
+sys.path.insert(0, '/root/hips')
+from definiciones import MAX_COLA_CORREOS
 
 
 def enviar_correo(receptor, subject, body):
@@ -26,12 +26,14 @@ def enviar_correo(receptor, subject, body):
     # Establecemos el servidor
     servidor = smtplib.SMTP('smtp.gmail.com: 587')
     servidor.starttls()
+
     servidor.login(correo, passwd)
     servidor.send_message(mensaje)
+
     del mensaje
     servidor.quit()
 
-    
+
 def reject_email(correo):
     cmd = "echo " + correo + "> /etc/postfix/access"
     p1 =subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
@@ -46,5 +48,5 @@ def verificar_cola():
         #se detiene el servicio de correo 
         p =subprocess.Popen("service postfix stop", stdout=subprocess.PIPE, shell=True)
         output, err = p.communicate()
-        reportar_alarma("Se supero el limite de la cola de correos")
-        reportar_prevencion("Se supero el limite de la cola de correos", "Se detuvo el servicio de correo")
+        #reportar_alarma("Se supero el limite de la cola de correos")
+        #reportar_prevencion("Se supero el limite de la cola de correos", "Se detuvo el servicio de correo")
